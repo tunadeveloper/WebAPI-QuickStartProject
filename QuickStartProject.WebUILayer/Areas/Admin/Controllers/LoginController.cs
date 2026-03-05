a2b455911b62e1416bad3c8918ac182ee1f41e22
tunadeveloper <tunabusiness25@gmail.com>
2026-03-05 17:28:14 +0300

    Updated

---
+++ QuickStartProject.WebUILayer/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickStartProject.WebUILayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoginController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
